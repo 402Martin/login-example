f2e5ce287f3f6d16edf7404375675b2d81904cfe
402Martin <402martin@gmail.com>
2023-09-29 16:26:58 -0300

    create login page

--- Backend/dto/UserDto.cs
+++ Backend/dto/UserDto.cs
@@ -1,14 +1,23 @@
-namespace Backend.Dto
-{
-  public struct Credentials
-  {
-    public readonly string Username { get; }
-    public readonly string Id { get; }
+namespace Backend.Dto;
 
-    public Credentials(string username, string id)
-    {
-      this.Username = username;
-      this.Id = id;
-    }
+public struct Credentials
+{
+  public readonly string Username { get; }
+  public readonly string Id { get; }
+
+  public Credentials(string username, string id)
+  {
+    this.Username = username;
+    this.Id = id;
   }
+}
+
+
+public class UserIn
+{
+  public string? Username { get; set; }
+  public string? Password { get; set; }
+
+  public UserIn()
+  { }
 }
